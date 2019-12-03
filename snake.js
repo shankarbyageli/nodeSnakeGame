02c73b9a7c55b12e5d760f77b8e5e4f76c41cb33
Shankara <=>
2019-12-03 09:52:09 +0530

    snake game in javascript

---
+++ snake.js
@@ -0,0 +1,187 @@
+let snake = [
+  {
+    dir : 1,
+    x : 4,
+    y : 1
+  },
+  {
+    dir : 1,
+    x : 3,
+    y : 1
+  },
+  {
+    dir : 1,
+    x : 2,
+    y : 1
+  },
+  {
+    dir : 1,
+    x : 1,
+    y : 1
+  }
+];
+
+let food = {
+  x : 20,
+  y: 20
+};
+
+let score = 0;
+
+process.stdin.setRawMode(true);
+
+const autoRunGame = function() {
+  const keyStroke = ['w','d','s','a'];
+  const headDir = snake[0].dir;
+  const autoGamePlay = gamePlay.bind(null, keyStroke[headDir]);
+  autoGamePlay();
+};
+
+const main = function() {
+  process.stdin.on('data', (chunk) => {
+    gamePlay(chunk);
+  });
+  setInterval(autoRunGame, 300);
+};
+
+const changeDirection = function(direction) {
+  snake[0].dir = direction;
+  moveIndir();
+  for(let i = snake.length - 1; i >= 0; i--) {
+    if(snake[i].dir != (snake[i-1] || snake[i]).dir) {
+      snake[i].dir = snake[i-1].dir;
+    }
+  }
+};
+
+const moveIndir = function() {
+  for(i in snake) {
+    if(snake[i].dir == 0) {
+      snake[i].y -= 1;
+    }
+    if(snake[i].dir == 1) {
+      snake[i].x += 1;
+    }
+    if(snake[i].dir == 2) {
+      snake[i].y += 1;
+    }
+    if(snake[i].dir == 3) {
+      snake[i].x -= 1;
+    }
+  }
+};
+
+const drawSnakeAndFood = function(snake, food) {
+  process.stdout.cursorTo(food.x, food.y);
+  console.log("*");
+  for(i in snake) {
+    process.stdout.cursorTo(snake[i].x, snake[i].y);
+    console.log('o');
+  }
+};
+
+const printScore = function(score) {
+  const rows = process.stdout.rows;
+  const columns = process.stdout.columns;
+  process.stdout.cursorTo(0, rows-2);
+  console.log("-".repeat(columns));
+  process.stdout.cursorTo(columns-12,rows-1);
+  console.log("Score : ",score);
+};
+
+const generateNewFood = function() {
+  x = Math.ceil(Math.random()*140);
+  y = Math.ceil(Math.random()*35);
+  food.x = x;
+  food.y = y;
+};
+
+const crashedToWall = function() {
+  const width = process.stdout.columns;
+  const height = process.stdout.rows - 2;
+  return snake.some(function(body) {
+    return (body.x <= 0 || body.y <= 0) || (body.x >= width || body.y >= height);
+  });
+};
+
+const isTouchBody = function() {
+  for(i in snake) {
+    if(i==0) continue;
+    if(snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+      return true;
+    }
+  }
+  return false;
+}
+
+const checkFoodAte = function() {
+  if(snake[0].x == food.x && snake[0].y == food.y) {
+    score += 5;
+    generateNewFood();
+    let newTail = getNewTailCords();
+    obj = {
+      dir : snake[snake.length-1].dir,
+      x : newTail.x,
+      y : newTail.y
+    }
+    snake.push(obj);
+  }
+};
+
+const getNewTailCords = function() {
+  let tail = {};
+  const tailDir = snake[snake.length-1].dir;
+  const tailPos = snake.length - 1;
+
+  if(tailDir == 0) {
+    tail.x = snake[tailPos].x;
+    tail.y = snake[tailPos].y+1;
+  }
+  if(tailDir == 1) {
+    tail.x = snake[tailPos].x-1;
+    tail.y = snake[tailPos].y;
+  }
+  if(tailDir == 2) {
+    tail.x = snake[tailPos].x
+    tail.y = snake[tailPos].y-1;
+  }
+  if(tailDir == 3) {
+    tail.x = snake[tailPos].x+1
+    tail.y = snake[tailPos].y;
+  }
+  return tail;
+};
+
+let gamePlay = function(input) {
+  if(input == 'q') {
+    process.exit(0);
+  }
+  input = input.toString();
+  const keyStroke = ['w','d','s','a'];
+  const moveDir = keyStroke.indexOf(input); 
+  if(moveDir != -1) {
+    changeDirection(moveDir);
+  }
+  console.clear();
+  
+  printScore(score);
+  drawSnakeAndFood(snake, food);
+  checkFoodAte(snake, food);
+
+  if(isTouchBody()) {
+    console.log("Score : ",score);
+    console.log("Touched snake body! Game over!");
+    process.exit(0);
+  }
+  if(crashedToWall()) {
+    console.log("Score : ",score);
+    console.log("You crashed to wall ! Game Over !");
+    process.exit(0);
+  }
+};
+
+main();
+
+process.stdin.on('end', () => {
+  console.log("exited the game !")
+});

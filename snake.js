edbb3c47a0f0fd2affdbd788d3f7ea9495df6a66
Shankara <=>
2019-12-29 22:10:36 +0530

    refactored snake.js

--- snake.js
+++ snake.js
@@ -1,30 +1,30 @@
-const {stdin, stdout} = process;
+const { stdin, stdout } = process;
 
 let snake = [
   {
-    dir : 1,
-    x : 4,
-    y : 1
+    dir: 1,
+    x: 4,
+    y: 1
   },
   {
-    dir : 1,
-    x : 3,
-    y : 1
+    dir: 1,
+    x: 3,
+    y: 1
   },
   {
-    dir : 1,
-    x : 2,
-    y : 1
+    dir: 1,
+    x: 2,
+    y: 1
   },
   {
-    dir : 1,
-    x : 1,
-    y : 1
+    dir: 1,
+    x: 1,
+    y: 1
   }
 ];
 
 let food = {
-  x : 20,
+  x: 20,
   y: 20
 };
 
@@ -33,7 +33,7 @@ let score = 0;
 stdin.setRawMode(true);
 
 const autoRunGame = function() {
-  const keyStroke = ['w','d','s','a'];
+  const keyStroke = ["w", "d", "s", "a"];
   const headDir = snake[0].dir;
   const autoGamePlay = gamePlay.bind(null, keyStroke[headDir]);
   autoGamePlay();
@@ -42,16 +42,16 @@ const autoRunGame = function() {
 const changeDirection = function(direction) {
   snake[0].dir = direction;
   moveIndir();
-  for(let i = snake.length - 1; i >= 0; i--) {
-    if(snake[i].dir != (snake[i-1] || snake[i]).dir) {
-      snake[i].dir = snake[i-1].dir;
+  for (let i = snake.length - 1; i >= 0; i--) {
+    if (snake[i].dir != (snake[i - 1] || snake[i]).dir) {
+      snake[i].dir = snake[i - 1].dir;
     }
   }
 };
 
 const moveIndir = function() {
   const operations = ["y -= 1", "x += 1", "y += 1", "x -= 1"];
-  for(i in snake) {
+  for (i in snake) {
     const operation = snake[i].dir;
     eval(`snake[i].${operations[operation]}`);
   }
@@ -60,24 +60,24 @@ const moveIndir = function() {
 const drawSnakeAndFood = function(snake, food) {
   stdout.cursorTo(food.x, food.y);
   stdout.write("*");
-  for(i in snake) {
+  for (i in snake) {
     stdout.cursorTo(snake[i].x, snake[i].y);
-    stdout.write('o');
+    stdout.write("o");
   }
 };
 
 const printScore = function(score) {
   const rows = stdout.rows;
   const columns = stdout.columns;
-  stdout.cursorTo(0, rows-2);
+  stdout.cursorTo(0, rows - 2);
   console.log("-".repeat(columns));
-  stdout.cursorTo(columns-12,rows-1);
-  console.log("Score : ",score);
+  stdout.cursorTo(columns - 12, rows - 1);
+  console.log("Score : ", score);
 };
 
 const generateNewFood = function() {
-  x = Math.ceil(Math.random()*140);
-  y = Math.ceil(Math.random()*35);
+  x = Math.ceil(Math.random() * 140);
+  y = Math.ceil(Math.random() * 35);
   food.x = x;
   food.y = y;
 };
@@ -85,76 +85,85 @@ const generateNewFood = function() {
 const crashedToWall = function() {
   const width = stdout.columns;
   const height = stdout.rows - 3;
-  return (snake[0].x < 0 || snake[0].y < 0) || (snake[0].x >= width || snake[0].y >= height);
+  return (
+    snake[0].x < 0 ||
+    snake[0].y < 0 ||
+    snake[0].x >= width || snake[0].y >= height
+  );
 };
 
 const isTouchBody = function() {
-  for(i in snake) {
-    if(i==0) continue;
-    if(snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+  for (i in snake) {
+    if (i == 0) continue;
+    if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
       return true;
     }
   }
   return false;
-}
+};
 
 const checkFoodAte = function() {
-  if(snake[0].x == food.x && snake[0].y == food.y) {
+  if (snake[0].x == food.x && snake[0].y == food.y) {
     score += 5;
     generateNewFood();
     let newTail = getNewTailCords();
     obj = {
-      dir : snake[snake.length-1].dir,
-      x : newTail.x,
-      y : newTail.y
-    }
+      dir: snake[snake.length - 1].dir,
+      x: newTail.x,
+      y: newTail.y
+    };
     snake.push(obj);
   }
 };
 
 const gameOverMsg = function(string) {
-  console.log("\nScore : ",score);
+  console.log("\nScore : ", score);
   console.log(string);
   console.log("Game Over !");
-  console.log('\x1B[?25h');
+  console.log("\x1B[?25h");
   process.exit(0);
 };
 
 const getNewTailCords = function() {
-  const operations = [["x","y+1"],["x-1","y"],["x","y-1"],["x+1","y"]];
+  const operations = [
+    ["x", "y+1"],
+    ["x-1", "y"],
+    ["x", "y-1"],
+    ["x+1", "y"]
+  ];
   let tail = {};
-  const tailDir = snake[snake.length-1].dir;
+  const tailDir = snake[snake.length - 1].dir;
   tail.x = eval(`snake[snake.length-1].${operations[tailDir][0]}`);
   tail.y = eval(`snake[snake.length-1].${operations[tailDir][1]}`);
   return tail;
 };
 
 let gamePlay = function(input) {
-  input == 'q' && gameOverMsg("You exited the game !");
+  input == "q" && gameOverMsg("You exited the game !");
   input = input.toString();
-  const keyStroke = ['w','d','s','a'];
-  const moveDir = keyStroke.indexOf(input); 
-  
-  if(moveDir != -1) {
-    if(Math.abs(moveDir-snake[0].dir)==2) {
-      return ;
+  const keyStroke = ["w", "d", "s", "a"];
+  const moveDir = keyStroke.indexOf(input);
+
+  if (moveDir != -1) {
+    if (Math.abs(moveDir - snake[0].dir) == 2) {
+      return;
     }
     changeDirection(moveDir);
   }
-  console.clear();  
+  console.clear();
   printScore(score);
   drawSnakeAndFood(snake, food);
   checkFoodAte(snake, food);
 
-  isTouchBody() && gameOverMsg("Touched the snake bady !");
+  isTouchBody() && gameOverMsg("Touched the snake body !");
   crashedToWall() && gameOverMsg("Crashed to wall !");
 };
 
 const main = function() {
-  stdin.on('data', (input) => {
+  stdin.on("data", input => {
     gamePlay(input);
   });
-  console.log('\x1B[?25l');
+  console.log("\x1B[?25l");
   setInterval(autoRunGame, 300);
 };
 
